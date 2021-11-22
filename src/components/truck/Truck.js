8684e9a2a78ebb45c7a222644c751f232a9c1960
Aasa-Christian <aasachristian@yahoo.com>
2021-11-22 18:31:04 -0500

    created truck component to hold trailor info

--- src/components/truck/Truck.js
+++ src/components/truck/Truck.js
@@ -1,9 +1,24 @@
 
 
 function Truck() {
+    const yard = [
+        {trailer: 14356,
+        load:[ [242,8]]},
+        {trailer: 202055,
+        load:[ [593,5],[528, 10]]}
+    ]
   return (
-    <div>
 
+    <div>
+        {yard.map((x) => {
+            console.log(x, "x")
+
+            return(
+                <section>
+                    <div>Trailor {x.trailer} </div>                   
+                </section>
+            )
+        })}
     </div>
   );
 }
