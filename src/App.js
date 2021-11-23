58a9ebb209860b377404bbf7be8cb57c3229d5a5
Aasa-Christian <aasachristian@yahoo.com>
2021-11-22 19:08:36 -0500

    added header tittle to appjs

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Truck from './components/truck/Truck';
 function App() {
   return (
     <div className="App">
+      <section>
+        <h1>Box Room Assist Tool</h1>
+        </section>
 <Truck/>
     </div>
   );
