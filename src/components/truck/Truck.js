3b5fc530590f8ec4764833f01a245efe351ad6fb
Aasa-Christian <aasachristian@yahoo.com>
2021-11-27 22:47:08 -0500

    added border and display flex to trailer list

--- src/components/truck/Truck.js
+++ src/components/truck/Truck.js
@@ -2,6 +2,7 @@ import Load from "./Load";
 
 
 function Truck() {
+    // yard dummy data
     const yard = [
         {trailer: 14356,
         load:[ [242,8]]},
@@ -11,15 +12,20 @@ function Truck() {
   return (
 
     <div>
+        {/* mao thrue the yard object container all trailers and load */}
         {yard.map((x, i) => {
             console.log(x, "x")
 
             return(
-                <section key = {i}>
-                    <div><h1>Trailor {x.trailer}</h1> </div> 
-                    <Load 
+                // key is array index
+                <section key = {i} style={{display: 'flex', justifyContent: "center"}}>
+                    {/* x.tralor is the trailer id */}
+                    <div style={{border: "solid black 3px", width: "50%", display: 'flex', justifyContent: "center"}}><h1>Trailer {x.trailer}</h1> </div> 
+                    {/* Load componenet to map and show each idividual load */}
+                    {/* <Load 
+                    // each load passed as props to load cmponenet
                     load = {x.load}
-                    />                  
+                    />                   */}
                 </section>
             )
         })}
