edff343d2a547260443273c8441ca495085ac21e
Aasa-Christian <aasachristian@yahoo.com>
2021-11-28 14:27:11 -0500

    added initialstate to reducer index

---
+++ src/reducers/index.js
@@ -0,0 +1,16 @@
+
+// This is the initial state prior to fetching
+const initialState = {
+trucks = [],
+loads = []
+    
+      };
+
+
+      export const reducer = (state = initialState, action) => {
+        switch (action.type){
+            default:
+                return state;
+        }
+
+      }
