7ccd42be4159b086d6187ac4a533f9c2fc65e8e6
Aasa-Christian <aasachristian@yahoo.com>
2021-11-28 14:18:22 -0500

    wrapped reducer store around appjs

--- src/index.js
+++ src/index.js
@@ -3,15 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux'
+import {createStore, applyMiddleware, compose} from 'redux'
+import {reducer} from './reducers'
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(thunk, logger))
+  );
+
+  ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    ,
+      document.getElementById('root')
+    );
+
 reportWebVitals();
