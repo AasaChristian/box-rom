c4086427caab0271c05a62ad4ed4273d8247b5c1
Aasa-Christian <aasachristian@yahoo.com>
2022-02-27 20:40:42 -0500

    created materials actions file, added fetch materials and add and delete boilerplate

---
+++ src/actions/materialActions.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+import {axiosAddress} from '../AxiosAddress'
+
+import {Fetch_Material_Success, Fetch_Material_Failed, Fetch_Material_Loading, Create_Material, Delete_Material} from './index'
+
+export const fetchMaterial = () => dispatch => {
+    dispatch({type: Fetch_Material_Loading})
+    axios.get(`${axiosAddress}/api/materials/all`)
+    .then(res => dispatch({type: Fetch_Material_Success, payload: res.data}))
+    .catch(error =>{
+        dispatch({type: Fetch_Material_Failed, payload: 'error'})
+        console.log(error, "fetchMaterial action error")
+    })
+            
+    };
+
+// export const createMaterial = (material) => dispatch => {
+//         axios.post(`${'x'}`, material)
+//         .then(res => {
+//             dispatch({type: Create_Material, payload: res.data})
+//         }).catch(error => console.log(error, "createMaterial action error"))
+//     }
+
+// export const deleteMaterial = (id) => dispatch => {
+//     axios.delete()
+//     .then(res => {
+//         dispatch({type: Delete_Material, payload: id})
+//     }).catch(error => console.log(error, "deleteMaterial action error"))
+// }
