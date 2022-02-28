8ec318b07831e831e83c20728cd3409ab612ed4a
Aasa-Christian <aasachristian@yahoo.com>
2022-02-27 20:43:40 -0500

    impoerted materials componenet, pulled fetchmaterials and materials from reducer store via redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import './App.css';
-import { useState } from "react";
-import Truck from './components/truck/Truck';
+import { useState, useEffect } from "react";
+import { connect } from 'react-redux';
+import Trailer from './components/trailer/Trailer';
+import MaterialBoard from './components/Materials/MaterialBoard'
+import { fetchMaterial } from './actions/materialActions';
 
-function App() {
+function App(props) {
 
+  const {materials, fetchMaterial} = props
+  useEffect(() => {
+    fetchMaterial()
+}, [])
+
+console.log(materials, "materials")
   return (
     <div className="App">
       <section>
         <h1>Box Room Assist Tool</h1>
         </section>
-<Truck
+{/* <Trailer
+/> */}
+
+<MaterialBoard
+materials = {materials}
 />
+
+
     </div>
   );
-}
 
-export default App;
+};
+
+const mapStateToProps = state => {
+  return {
+        materials: state.materials,
+        loading: state.loading
+  };
+};
+
+export default connect(mapStateToProps, {fetchMaterial})(App);
