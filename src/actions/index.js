385ed1a7ef0466f7a9d40b4bbe1b7395553d5c8f
Aasa-Christian <aasachristian@yahoo.com>
2022-03-06 19:15:13 -0500

    exported creatematerials action from action index

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,3 +5,5 @@ export const Fetch_Material_Success = 'Fetch_Material_Success';
 export const Fetch_Material_Failed = 'Fetch_Material_Failed';
 
 export const Fetch_Material_Loading = 'Fetch_Material_Loading';
+
+export const Create_Material = 'Create_Material'
