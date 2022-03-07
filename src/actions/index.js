2e014488a24ad8d75182a46180304aaa7c1c5073
Aasa-Christian <aasachristian@yahoo.com>
2022-03-06 22:44:45 -0500

    export delete material action from index

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,3 +7,5 @@ export const Fetch_Material_Failed = 'Fetch_Material_Failed';
 export const Fetch_Material_Loading = 'Fetch_Material_Loading';
 
 export const Create_Material = 'Create_Material'
+
+export const Delete_Material = 'Delete_Material'
