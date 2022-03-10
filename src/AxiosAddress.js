22a3683f03bce886db8de7eb924a8136139298ad
Aasa-Christian <aasachristian@yahoo.com>
2022-03-10 00:54:25 -0500

    updated axioaddress

--- src/AxiosAddress.js
+++ src/AxiosAddress.js
@@ -1,6 +1,6 @@
 // export const axiosAddress = "http://localhost:5000"
 
-export const axiosAddress = "https://box-room.herokuapp.com/"
+export const axiosAddress = "https://box-room.herokuapp.com"
 
 
 
