aa03769de4c85eddf3df6663272fceffd3b81135
Aasa-Christian <aasachristian@yahoo.com>
2022-03-13 23:27:44 -0400

    added trailer actions and reducer cases

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,3 +9,15 @@ export const Fetch_Material_Loading = 'Fetch_Material_Loading';
 export const Create_Material = 'Create_Material'
 
 export const Delete_Material = 'Delete_Material'
+
+// Trailer Actions
+
+export const Fetch_Trailer_Success = 'Fetch_Trailer_Success';
+
+export const Fetch_Trailer_Failed = 'Fetch_Trailer_Failed';
+
+export const Fetch_Trailer_Loading = 'Fetch_Trailer_Loading';
+
+export const Create_Trailer = 'Create_Trailer'
+
+export const Delete_Trailer = 'Delete_Trailer'
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 
-import {Fetch_Material_Loading, Fetch_Material_Success, Fetch_Material_Failed, Create_Material, Delete_Material} from '../actions/index'
+import {Fetch_Material_Loading, Fetch_Material_Success, Fetch_Material_Failed, Create_Material, Delete_Material, Fetch_Trailer_Loading, Fetch_Trailer_Success, Create_Trailer, Delete_Trailer} from '../actions/index'
 
 
 // This is the initial state prior to fetching
@@ -41,6 +41,28 @@ materials: []
             return {
               ...state,
               materials: [...state.materials.filter(material => material.id !== action.payload)]
+            };
+
+            case Fetch_Trailer_Loading:
+              return{
+                ...state,
+                loading: true
+              };
+            case Fetch_Trailer_Success:
+              return {
+                ...state,
+                trailers: action.payload,
+                loading: false
+              };
+        case Create_Trailer:
+                return {
+                  ...state,
+                  trailers: [...state.trailers, action.payload[0]]
+                };
+        case Delete_Trailer:
+            return {
+              ...state,
+              trailers: [...state.trailers.filter(trailer => trailer.id !== action.payload)]
             };
             default:
                 return state;
