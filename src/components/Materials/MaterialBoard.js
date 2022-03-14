f1c3081265fe15e7f440362907f9d52097092e4d
Aasa-Christian <aasachristian@yahoo.com>
2022-03-13 23:27:08 -0400

    added helper functions to component

--- src/components/Materials/MaterialBoard.js
+++ src/components/Materials/MaterialBoard.js
@@ -5,7 +5,8 @@ import {
   createMaterial,
   deleteMaterial,
 } from "../../actions/materialActions";
-import Pulse from "react-reveal/Pulse";
+import UpdateBoard from "../MultiUse/updateBoard";
+import CreateNew from "../MultiUse/createNew";
 
 function MaterialBoard(props) {
   const {
@@ -23,8 +24,8 @@ function MaterialBoard(props) {
   console.log(materials, "is it materials?");
 
   const [newMaterial, setNewMaterial] = useState({
-    tagNumber: " ",
-    materialname: " ",
+    tagNumber: "",
+    name: "",
   });
   const [selectedMaterial, setSelectedMaterial] = useState(0);
 
@@ -36,149 +37,36 @@ function MaterialBoard(props) {
     e.preventDefault();
     const newMaterialObj = {
       tagNumber: newMaterial.tagNumber,
-      materialname: newMaterial.materialname,
+      materialname: newMaterial.name,
     };
     createNewMaterial(newMaterialObj);
     setNewMaterial({
-      tagNumber: " ",
-      materialname: " ",
+      tagNumber: "",
+      name: "",
     });
   };
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column ",
-        alignItems: "center",
-        width: "100%",
-      }}
-    >
-      <section>
-        <h1>Materials</h1>
-      </section>
-      <div style={{ display: "flex", justifyContent: "center" }}>
-        {loading && (
-          <Pulse delay={500} forever={true}>
-            <h1 style={{ color: "black", fontSize: "500%", position: "fixed" }}>
-              LOADING
-            </h1>
-          </Pulse>
-        )}
-      </div>
-      <section>
-        <div>
-          <h3>Create New Material</h3>
-        </div>
-        <div style={{ display: "flex", position: "fixed" }}>
-          <h3>{newMaterial.tagNumber}</h3> <h3>{newMaterial.materialname}</h3>{" "}
-        </div>
+    
+    <div > 
+<section>
+<CreateNew
+loading={loading}
+handleChange={handleChange}
+createNewTrailer={createNewMaterial}
+send={sendMaterial}
+title='Materials'
+newTable={newMaterial}
+/>
+  </section>
 
-        <div>
-          <form onSubmit={sendMaterial}>
-            <input
-              type="text"
-              name="tagNumber"
-              placeholder="TagNumber"
-              value={newMaterial.tagNumber}
-              onChange={handleChange}
-            />
-            <input
-              type="text"
-              name="materialname"
-              placeholder="MaterialName"
-              value={newMaterial.materialname}
-              onChange={handleChange}
-            />
-            <button>ADD</button>
-          </form>
-        </div>
-      </section>
-      <section
-        style={{
-          display: "flex",
-          flexDirection: "column ",
-          justifyContent: "space-around",
-          width: "70%",
-          height: "100%",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            flexDirection: "row",
-            width: "100%",
-          }}
-        >
-          <h4 style={{ borderBottom: "2px solid black" }}>Tag #</h4>
-          <h4 style={{ borderBottom: "2px solid black" }}>Name</h4>
-        </div>
-        {materials.map((material) => {
-          const { materialname, tagNumber, id } = material;
-
-          const selectMaterial = (e) => {
-            e.preventDefault();
-            if (selectedMaterial === null) {
-              setSelectedMaterial(id);
-            } else {
-              setSelectedMaterial(null);
-            }
-          };
-          const removeMaterial = (e) => {
-            e.preventDefault();
-            deleteMaterial(id);
-          };
-          return (
-            <section
-              key={id}
-              style={
-                {
-                  display: "flex",
-                  justifyContent: "space-between",
-                  flexDirection: "row",
-                  width: "100%",
-                } |
-                (selectedMaterial === id)
-                  ? { backgroundColor: "#07fcff1a", borderRadius: "10%", border: "2px solid #00000038" }
-                  : { display: "inherit" }
-              }
-              onClick={selectMaterial}
-            >
-              <section
-                style={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  width: "100%",
-                }}
-              >
-                <div>
-                  <h5>{tagNumber}</h5>
-                </div>
-                <div>
-                  <h5>{materialname}</h5>
-                </div>
-              </section>
-              <section
-                style={
-                  selectedMaterial === id
-                    ? { display: "flex", justifyContent: "center" }
-                    : { display: "none" }
-                }
-              >
-                <form
-                  style={{width: "100%"}}
-                >
-                  <div style={{ display: "flex", justifyContent: "space-evenly", width: "100%" }}>
-                    {" "}
-                    <button onClick={removeMaterial}>Delete</button>
-                    <button >Update</button>
-                  </div>
-                </form>
-              </section>
-            </section>
-          );
-        })}
-      </section>
+  <section style={{display: 'flex', justifyContent: 'center'}}>
+  <UpdateBoard
+table={materials}
+deleteTable={deleteMaterial}
+selected={selectedMaterial}
+setSelected={setSelectedMaterial}
+/> 
+</section>
     </div>
   );
 }
